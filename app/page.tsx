531e7164643098541f090be6e6aacd9ed238d313
yohan <yohan@adelios.fr>
2024-05-13 21:28:09 +0200

    chore: setup de clerk

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import SignIn from '@/components/SignIn/SignIn';
 
 export default function Home() {
 	return (
-		<div className="bg-gray-100 w-full h-[100vh] flex justify-center items-center ">
-			<SignIn />
-		</div>
+		<div className="border-2 border-red-500 bg-gray-100 w-full h-[100vh] flex justify-center items-center "></div>
 	);
 }
