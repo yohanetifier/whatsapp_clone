4a1f0a3a3771383680579659b577425682d3a660
yohan <yohan@adelios.fr>
2024-05-15 22:28:39 +0200

    feat: récupération des identifiants de l'user

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import SignIn from '@/components/SignIn/SignIn';
+import { auth, currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+	const user = await currentUser();
+	if (!user) {
+		redirect('/sign-in');
+	}
 	return (
-		<div className="border-2 border-red-500 bg-gray-100 w-full h-[100vh] flex justify-center items-center "></div>
+		<div className="border-2 border-red-500 bg-gray-100 w-full h-[100vh] flex justify-center items-center ">
+			<SignIn />
+		</div>
 	);
 }
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,11 @@
 import { SignIn } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs/server';
 
 export default function Page() {
+	const { userId } = auth();
+	if (userId) {
+		console.log('userId', userId);
+	}
 	return (
 		<div className="flex justify-center items-center h-[100vh]">
 			<SignIn path="/sign-in" />
