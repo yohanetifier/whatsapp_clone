0e650c7499cafaae164d5c8db1925d2b10ad39e5
yohan <yohan@adelios.fr>
2024-05-22 22:15:02 +0200

    chore: add topbar.test.sx

---
+++ components/TopBar/TopBar.test.tsx
@@ -0,0 +1,7 @@
+import '@testing-library/jest-dom/';
+import { render, screen } from '@testing-library/react';
+import TopBar from './TopBar';
+
+describe('TopBar', () => {
+	it('should render without crash', () => {});
+});
