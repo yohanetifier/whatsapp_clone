93205a21a3f9c506daad81035ec8e952bbac291e
yohan <yohan@adelios.fr>
2024-05-25 11:59:21 +0200

    refactor: add test for MessageBar

--- components/MessageBar/MessageBar.test.tsx
+++ components/MessageBar/MessageBar.test.tsx
@@ -3,5 +3,15 @@ import { render, screen } from '@testing-library/react';
 import MessageBar from './MessageBar';
 
 describe('MessageBar', () => {
-	it('should first', () => {});
+	it('should have 4 action buttons ', () => {
+		render(<MessageBar />);
+		const buttons = screen.getAllByRole('button');
+		expect(buttons).toHaveLength(4);
+	});
+	it('should have a input of type text', () => {
+		render(<MessageBar />);
+		const input = screen.getByRole('textbox');
+		expect(input).toBeInTheDocument();
+		expect(input).toHaveAttribute('type', 'text');
+	});
 });
